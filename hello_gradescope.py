0909287bb838cab3e71a0f4df208f35eaf27459d
“Daniel <danielgacosta6@gmail.com>
2023-01-09 15:22:17 -0600

    this is my assignment 1

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Daniel Acosta
+# OSU Email: acostad@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 1, gradescope test
+# Due Date: January 20, 2023
+# Description: This is my first assignment for gradescope.
+
+
+def gradescope_test():
+    return 42
